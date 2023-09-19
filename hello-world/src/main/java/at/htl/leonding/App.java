d8fef6f5862e74380f43e06ee4dd88de7628a111
M0Wagner <moritz.wagner06@gmx.at>
2023-09-19 09:21:19 +0200

    siu

--- hello-world/src/main/java/at/htl/leonding/App.java
+++ hello-world/src/main/java/at/htl/leonding/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Its Messiiiiiiiii!" );
+        System.out.println( "Itsa Meeeee Mario!" );
     }
 }
